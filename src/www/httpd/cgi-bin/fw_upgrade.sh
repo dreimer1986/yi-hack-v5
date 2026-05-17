8a9dac37defa1561277ec39872dc3d33432f4460
Daniel Reimer <daniel.reimer@reactos.org>
2026-05-17 08:07:58 +0200

    Update firmware upgrade script for slim version
    If I fix a url problem, then I have to do it for all occurrences of course...

--- src/www/httpd/cgi-bin/fw_upgrade.sh
+++ src/www/httpd/cgi-bin/fw_upgrade.sh
@@ -22,7 +22,7 @@ if [ "$VAL" == "info" ] ; then
 
     FW_VERSION=`cat /tmp/sd/yi-hack-v5/version`
     LATEST_FW=`wget -O - https://api.github.com/repos/pawelma/yi-hack-v5-slim/releases/latest 2>&1 | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'`
-    PRERELEASE_FW=`wget -O - https://api.github.com/repos/pawelma/yi-hack-v5/releases 2>&1 | grep -B 4 '"prerelease": true' | awk -F '"' '{print $4; exit}'`
+    PRERELEASE_FW=`wget -O - https://api.github.com/repos/pawelma/yi-hack-v5-slim/releases 2>&1 | grep -B 4 '"prerelease": true' | awk -F '"' '{print $4; exit}'`
 	
     printf "{\n"
     printf "\"%s\":\"%s\",\n" "fw_version"       "$FW_VERSION"
@@ -61,14 +61,14 @@ elif [ "$VAL" == "upgrade" ] ; then
 #        mv /tmp/sd/${MODEL_SUFFIX}_x.x.x.tgz /tmp/sd/${MODEL_SUFFIX}/${MODEL_SUFFIX}_x.x.x.tgz
         LATEST_FW="x.x.x"
     else
-        LATEST_FW=`wget -O -  https://api.github.com/repos/pawelma/yi-hack-v5/releases/latest 2>&1 | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'`
+        LATEST_FW=`wget -O -  https://api.github.com/repos/pawelma/yi-hack-v5-slim/releases/latest 2>&1 | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'`
         if [ "$FW_VERSION" == "$LATEST_FW" ]; then
             printf "Content-type: text/html\r\n\r\n"
             printf "No new firmware available."
             exit
         fi
         
-        wget https://github.com/pawelma/yi-hack-v5/releases/download/$LATEST_FW/${MODEL_SUFFIX}_${LATEST_FW}.tgz
+        wget https://github.com/pawelma/yi-hack-v5-slim/releases/download/$LATEST_FW/${MODEL_SUFFIX}_${LATEST_FW}.tgz
         
         if [ ! -f ${MODEL_SUFFIX}_${LATEST_FW}.tgz ]; then
             printf "Content-type: text/html\r\n\r\n"
@@ -129,14 +129,14 @@ elif [ "$VAL" == "preupgrade" ] ; then
 #        mv /tmp/sd/${MODEL_SUFFIX}_x.x.x.tgz /tmp/sd/${MODEL_SUFFIX}/${MODEL_SUFFIX}_x.x.x.tgz
         PRERELEASE_FW="x.x.x"
     else
-        PRERELEASE_FW=`wget -O - https://api.github.com/repos/pawelma/yi-hack-v5/releases 2>&1 | grep -B 4 '"prerelease": true' | awk -F '"' '{print $4; exit}'`
+        PRERELEASE_FW=`wget -O - https://api.github.com/repos/pawelma/yi-hack-v5-slim/releases 2>&1 | grep -B 4 '"prerelease": true' | awk -F '"' '{print $4; exit}'`
         if [ "$FW_VERSION" == "$PRERELEASE_FW" ]; then
             printf "Content-type: text/html\r\n\r\n"
             printf "No new firmware available."
             exit
         fi
 
-        wget https://github.com/pawelma/yi-hack-v5/releases/download/$PRERELEASE_FW/${MODEL_SUFFIX}_${PRERELEASE_FW}.tgz
+        wget https://github.com/pawelma/yi-hack-v5-slim/releases/download/$PRERELEASE_FW/${MODEL_SUFFIX}_${PRERELEASE_FW}.tgz
         
         if [ ! -f ${MODEL_SUFFIX}_${PRERELEASE_FW}.tgz ]; then
             printf "Content-type: text/html\r\n\r\n"
